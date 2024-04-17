fbb5fdb255e65954270953bb430ee692ec4afe58
jronnefalk <julia@ronnefalk.se>
2024-04-17 14:23:57 +0200

    fix

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-
 data = pd.read_csv("Data/EyeTrack-raw.tsv", delimiter="\t")
 
 # Print the first few rows to check the data
@@ -10,22 +9,37 @@ data = pd.read_csv("Data/EyeTrack-raw.tsv", delimiter="\t")
 
 # Plotting the data for gazepoints in x and y
 plt.scatter(data["GazePointX(px)"], data["GazePointY(px)"])
-plt.xlabel("X Coordinate (pixels)")
-plt.ylabel("Y Coordinate (pixels)")
-plt.title("Visualization of Gaze Points")
+plt.xlabel("X coordinate of gaze point in pixels")
+plt.ylabel("Y coordinate of gaze point in pixels")
+plt.show()
+
+# Scatter plot with adjusted alpha
+plt.scatter(data["GazePointX(px)"], data["GazePointY(px)"], alpha=0.25)
+plt.xlabel("X coordinate of gaze point in pixels")
+plt.ylabel("Y coordinate of gaze point in pixels")
 plt.show()
 
 
-# Assuming you decide to find, e.g., 5 clusters
-kmeans = KMeans(n_clusters=4)
-data["cluster"] = kmeans.fit_predict(data[["GazePointX(px)", "GazePointY(px)"]])
-
-# Plotting clusters
-plt.scatter(
-    data["GazePointX(px)"], data["GazePointY(px)"], c=data["cluster"], cmap="viridis"
+# Normalize the gaze duration for visibility
+data["NormalizedDuration"] = (
+    data["GazeEventDuration(mS)"] / data["GazeEventDuration(mS)"].max()
 )
-plt.xlabel("X Coordinate (pixels)")
-plt.ylabel("Y Coordinate (pixels)")
-plt.title("Clustered Gaze Points")
-plt.colorbar(label="Cluster")
+
+# Apply linear scaling with a controlled minimum size
+data["PointSizes"] = (
+    data["NormalizedDuration"] * 500
+)  # Scale factor adjusted for visibility
+data["PointSizes"] = data["PointSizes"].apply(
+    lambda x: max(x, 10)
+)  # Set a minimum size
+
+# Scatter plot where the size of each point is based on the linearly scaled gaze duration
+plt.scatter(
+    data["GazePointX(px)"],
+    data["GazePointY(px)"],
+    alpha=0.25,
+    s=data["PointSizes"],
+)
+plt.xlabel("X coordinate of gaze point in pixels")
+plt.ylabel("Y coordinate of gaze point in pixels")
 plt.show()
