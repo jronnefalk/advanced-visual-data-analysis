1b06fb7c7dcf23ae3cfb8e229c534b5a391cba41
jronnefalk <julia@ronnefalk.se>
2024-04-19 11:37:35 +0200

    loading images

--- Labb 3/main.py
+++ Labb 3/main.py
@@ -9,9 +9,19 @@ def load_images_from_folder(folder):
     images = []
     filenames = []
     for filename in os.listdir(folder):
-        if filename.endswith(".png"):  # Assuming all images are in PNG format
+        if filename.endswith(".jpg"):
             img = cv2.imread(os.path.join(folder, filename))
             if img is not None:
                 images.append(img)
                 filenames.append(filename)
     return images, filenames
+
+
+# Specify the path to your folder containing images
+folder_path = "./images"  # Replace with your actual folder path
+loaded_images, loaded_filenames = load_images_from_folder(folder_path)
+
+# Display the filenames of the loaded images
+print("Loaded images:")
+for filename in loaded_filenames:
+    print(filename)
